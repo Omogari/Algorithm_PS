fb0d64c179089165226acaee7086d6d1eef622d5
wbluke <pwb0127@naver.com>
2018-11-07 23:37:14 +0900

    Update PICNIC <1>

--- CH06/CH06/PICNIC.cpp
+++ CH06/CH06/PICNIC.cpp
@@ -1,8 +1,34 @@
 #include<iostream>
 using namespace std;
 
+int student, soulmateCount;
+bool soulmate[10][10];
+
+int countPairing(bool matching[10]) {
+
+
+
+}
+
 int main(void) {
 
+	int num, f1, f2;
+	cin >> num;
+
+	for (int i = 0; i < num; i++) {
+
+		cin >> student >> soulmateCount;
+		
+		memset(soulmate, false, sizeof(soulmate));
+
+
+		for (int j = 0; j < soulmateCount; j++) {
+			cin >> f1 >> f2;
+			soulmate[f1][f2] = soulmate[f2][f1] = true;
+
+		}
+
+	}
 
 
 	return 0;
