34582ae16b715136c608d5726315a0545f41367e
wbluke <pwb0127@naver.com>
2018-11-13 20:58:04 +0900

    [#3] CLOCKSYNC <2>
    * push, checkTwelve, solve, main함수 구현.

--- CH06/CH06/CLOCKSYNC.cpp
+++ CH06/CH06/CLOCKSYNC.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
+const int INF = 987654321;
 const int clockSwitch[10][16] = {
 	{ 1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0 },
 	{ 0,0,0,1,0,0,0,1,0,1,0,1,0,0,0,0 },
@@ -14,6 +16,41 @@ const int clockSwitch[10][16] = {
 	{ 0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0 },
 	{ 0,0,0,1,1,1,0,0,0,1,0,0,0,1,0,0 }
 };
+
+vector<int> push(vector<int> &clockes, const int sw[16]) {
+	for (int i = 0; i < 16; i++) {
+		clockes[i] += sw[i];
+		if (clockes[i] == 4) {
+			clockes[i] = 0;
+		}
+	}
+	return clockes;
+}
+
+bool checkTwelve(vector<int> &clockes) {
+	bool ret = true;
+	for (int i = 0; i < 16; i++) {
+		if (clockes[i] != 3) {
+			ret = false;
+			break;
+		}
+	}
+	return ret;
+}
+
+int solve(vector<int> &clockes, const int clockSwitch[10][16], int sw) {
+	int ret = 0;
+	if (sw == 10) return ret;
+
+	//base case
+	if (checkTwelve(clockes)) return 1;
+
+	for (int count = 0; count < 4; count++) {
+		ret = min(ret, count + solve(clockes, clockSwitch, sw+1));
+		push(clockes, clockSwitch[sw]);
+	}
+	return ret;
+}
 
 int main(void) {
 
@@ -29,7 +66,13 @@ int main(void) {
 			clockes[i] = clockes[i] / 3 - 1;
 		}
 
-		
+		int ret = solve(clockes, clockSwitch, 0);
+		if (ret >= INF) {
+			cout << -1 << endl;
+		}
+		else {
+			cout << ret << endl;
+		}
 
 	}
 
