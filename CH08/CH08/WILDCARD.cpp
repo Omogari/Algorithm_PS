ca376a6509d31568ecf39eb35dad71c216014eb6
wbluke <pwb0127@naver.com>
2018-12-02 22:56:32 +0900

    [#8] WILDCARD <2>
    * match함수 구현. (+반환형 수정)
    * cache 배열 크기 수정.
    * 정렬 sort함수 구현.

--- CH08/CH08/WILDCARD.cpp
+++ CH08/CH08/WILDCARD.cpp
@@ -1,30 +1,36 @@
 #include<iostream>
 #include<string>
 #include<cstring>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-int cache[100][100];
+int cache[101][101];
 string W, S;
 
-bool match(int w, int s) {
+int match(int w, int s) {
+
+	//memoization
+	int &ret = cache[w][s];
+	if (ret != -1) return ret;
 
 	while (w < W.size() && s < S.size() && (W[w] == S[s] || W[w] == '?')) {
 		w++;
 		s++;
 	}
 
-	//memoization
-	int &ret = cache[w][s];
-	if (ret != -1) return ret;
-
 	if (w == W.size()) {
-		return ret = (W.size() == S.size());
+		return ret = (s == S.size());
 	}
 
 	if (W[w] == '*') {
-		//? if(match(w+1,s))
+		for (int skip = 0; skip + s <= S.size(); skip++) {
+			if (match(w + 1, s + skip)) {
+				return ret = 1;
+			}
+		}
 	}
-
+	return ret = 0;
 }
 
 int main(void) {
@@ -32,15 +38,20 @@ int main(void) {
 	int num, n;
 	cin >> num;
 	for (int a = 0; a < num; a++) {
+		vector<string> v;
 		cin >> W;	//wildcard pattern
 
 		cin >> n;	//num of filenames
 		for (int b = 0; b < n; b++) {
 			cin >> S;	//filename
-
+				
 			memset(cache, -1, sizeof(cache));
-
-
+			if (match(0, 0) == 1)
+				v.push_back(S);
+		}
+		sort(v.begin(), v.end());
+		for (int i = 0; i < v.size(); i++) {
+			cout << v[i] << endl;
 		}
 	}
 	return 0;
