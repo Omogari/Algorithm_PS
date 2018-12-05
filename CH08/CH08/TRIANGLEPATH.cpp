1b915466a5f00b4f99b6103fb22763a0fa7ca70b
wbluke <pwb0127@naver.com>
2018-12-06 00:25:51 +0900

    [#9] TRIANGLEPATH <1>
    * main함수와 memoization을 활용한 findMaxSumPath 함수 구현.

--- CH08/CH08/TRIANGLEPATH.cpp
+++ CH08/CH08/TRIANGLEPATH.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
+#include<cstring>
+#include<algorithm>
 using namespace std;
 
 int triangle[100][100];
+int cache[100][100];
+int n;	//size of triangle
+
+int findMaxSumPath(int y, int x) {
+	//memoization
+	int &ret = cache[y][x];
+	if (ret != 0) return ret;
+
+	//base case
+	if (y == n - 1) return ret = triangle[y][x];
+
+	return ret = triangle[y][x] + max(findMaxSumPath(y + 1, x), findMaxSumPath(y + 1, x + 1));
+}
 
 int main(void) {
 
@@ -9,10 +24,16 @@ int main(void) {
 	cin >> num;
 	for (int a = 0; a < num; a++) {
 
-		
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				cin >> triangle[i][j];
+			}
+		}
+		//initialize cache
+		memset(cache, 0, sizeof(cache));
 
-
-
+		cout << findMaxSumPath(0, 0) << endl;
 	}
 	return 0;
 }
