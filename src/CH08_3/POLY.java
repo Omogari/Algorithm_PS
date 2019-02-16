33437f8661f10433fc5bf5064f4aed5aeaf24e6f
wbluke <pwb0127@naver.com>
2019-02-16 20:21:15 +0900

    [#18] POLY<1>
    * main 함수 구현.

---
+++ src/CH08_3/POLY.java
@@ -0,0 +1,18 @@
+package CH08_3;
+
+import java.util.*;
+
+public class POLY {
+
+    public static void main(String[] args) {
+        POLY poly = new POLY();
+        Scanner scan = new Scanner(System.in);
+
+        int loop = scan.nextInt();
+        while(loop-- > 0) {
+            int n = scan.nextInt();
+
+            
+        }
+    }
+}
