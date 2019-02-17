ee1d2e595f0ce92fa62091fc041e4c2efb254734
wbluke <pwb0127@naver.com>
2019-02-18 07:04:43 +0900

    [#18] POLY<2>
    * 정사각형의 개수와 첫 줄의 길이를 받는 countMonoPoly 메소드 구현.
    * 반복문과 재귀를 통해 정답 도출.

--- src/CH08_3/POLY.java
+++ src/CH08_3/POLY.java
@@ -3,16 +3,43 @@ package CH08_3;
 import java.util.*;
 
 public class POLY {
+    private static final int MOD = 10 * 1000 * 1000;
+    private int[][] cache;
+
+    public int countMonoPoly(int squares, int firstLine) {
+        // base case
+        if (squares == firstLine) return 1;
+
+        // memoization
+        if (cache[squares][firstLine] != -1) return cache[squares][firstLine];
+
+        int ret = 0;
+        for (int i = 1; i <= squares - firstLine; i++) {
+            ret += (firstLine + i - 1) * countMonoPoly(squares - firstLine, i) % MOD;
+        }
+        return cache[squares][firstLine] = ret % MOD;
+    }
 
     public static void main(String[] args) {
         POLY poly = new POLY();
         Scanner scan = new Scanner(System.in);
 
+        poly.cache = new int[101][101];
+
         int loop = scan.nextInt();
         while(loop-- > 0) {
+            // cache init
+            for (int[] arr : poly.cache) {
+                Arrays.fill(arr, -1);
+            }
+
             int n = scan.nextInt();
 
-
+            int polyomino = 0;
+            for (int i = 1; i <= n; i++) {
+                polyomino += poly.countMonoPoly(n, i);
+            }
+            System.out.println(polyomino % MOD);
         }
     }
 }
