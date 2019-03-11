e44d0bae288922332d057c9510fea0ceda6ed5e2
wbluke <pwb0127@naver.com>
2019-03-11 11:03:08 +0900

    [#23] TRAVERSAL <2>
    * generatePostorder 구현 완료.

--- src/CH21/TRAVERSAL.java
+++ src/CH21/TRAVERSAL.java
@@ -9,61 +9,46 @@ public class TRAVERSAL {
     private int[] inorder;
     private ArrayList<Integer> postorder;
 
-    public void generatePostorder(int preFrom, int preTo, int inFrom, int inTo){ //int[] preorder, int[] inorder) {
-        int root = preorder[preFrom];
+    public void generatePostorder(int[] preorder, int[] inorder) {
+
+        // base case
+        if (preorder.length == 0) {
+            return;
+        }
+
+        int root = preorder[0];
 
         // find root node index in inorder traversal
         int rootIdx = -1;
-        for (int i = inFrom; i < inTo; i++) {
+        for (int i = 0; i < inorder.length; i++) {
             if (inorder[i] == root) {
                 rootIdx = i;
                 break;
             }
         }
-        // next subPreorder in left Tree : preFrom + 1 ~ rootIdx + 1
-        // next subInorder in left Tree : preFrom ~ rootIdx
-        // next subPreorder in right Tree : rootIdx + 1 ~ preTo
-        // next subInorder in right Tree : rootIdx + 1 ~  ~ inTo
 
-//        1
-//        7
-//        27 16 9 12 54 36 72
-//        9 12 16 27 36 54 72
+        // next subPreorder by left Tree : 1 ~ rootIdx + 1
+        // next subInorder by left Tree : 0 ~ rootIdx
+        // next subPreorder by right Tree : rootIdx + 1 ~ preorder.length
+        // next subInorder by right Tree : rootIdx + 1 ~  ~ inorder.length
 
-        // 12 9 16
+        int[] leftSubPreorder = new int[rootIdx];
+        int[] leftSubInorder = new int[rootIdx];
+        int[] rightSubPreorder = new int[preorder.length - rootIdx - 1];
+        int[] rightSubInorder = new int[inorder.length - rootIdx - 1];
 
-        // gen(0, 7, 0, 7)
-        // root : 27, rootIdx : 3
-            // gen(1, 4, 0, 3)
-            // root : 16, rootIdx : 2
-                // gen(2, 4, 0, 2)
-                // root : 9, rootIdx : 0 => pass
-                // pass
-            // gen(4, 7, 4, 7)
-            // root : 54, rootIdx : 5
-                // gen(5, 9)
-
-        // length
-        int sizeOfLTree = rootIdx - preFrom;
+        System.arraycopy(preorder, 1, leftSubPreorder, 0, leftSubPreorder.length);
+        System.arraycopy(inorder, 0, leftSubInorder, 0, leftSubInorder.length);
+        System.arraycopy(preorder, rootIdx + 1, rightSubPreorder, 0, rightSubPreorder.length);
+        System.arraycopy(inorder, rootIdx + 1, rightSubInorder, 0, rightSubInorder.length);
 
         // left subTree
-        if (preFrom == rootIdx) {
-            postorder.add(preorder[preFrom]); // leaf node
-            //return;
-        }else if (preFrom < rootIdx) {
-            generatePostorder(preFrom + 1, preFrom + rootIdx, inFrom, rootIdx);
-        }
+        generatePostorder(leftSubPreorder, leftSubInorder);
 
         // right subTree
-        if (rootIdx + 1 == inTo - 1) {
-            postorder.add(inorder[rootIdx + 1]); // leaf node
-            //return;
-        }else if (rootIdx + 1 < inTo - 1) {
-            generatePostorder(rootIdx + 1, preTo, rootIdx + 1, inTo);
-        }
+        generatePostorder(rightSubPreorder, rightSubInorder);
 
         postorder.add(root);
-
     }
 
     public static void main(String[] args) {
@@ -86,10 +71,11 @@ public class TRAVERSAL {
                 traversal.inorder[i] = scan.nextInt();
             }
 
-            traversal.generatePostorder(0, nodes, 0, nodes);
+            traversal.generatePostorder(traversal.preorder, traversal.inorder);
             for (int node : traversal.postorder){
-                System.out.println(node + " ");
+                System.out.print(node + " ");
             }
+            System.out.println();
         }
     }
 }
